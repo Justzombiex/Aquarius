957651ab69f38f9c9b52999ab9d286d402ce51e6
Brian Michel <bmoralescutting@gmail.com>
2025-03-18 16:47:59 -0400

    Implementando servicios Angular

--- Aquarius.Angular/src/app/services/farm.service.ts
+++ Aquarius.Angular/src/app/services/farm.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Farm } from '../models/farm.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FarmService {
+  private apiUrl = 'https://localhost:5001/api/Farms';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getFarms(): Observable<Farm[]> {
+    return this.http.get<Farm[]>(this.apiUrl);
+  }
+
+  getFarm(id: string): Observable<Farm> {
+    return this.http.get<Farm>(`${this.apiUrl}/${id}`);
+  }
+
+  createFarm(farm: Farm): Observable<Farm> {
+    return this.http.post<Farm>(this.apiUrl, farm);
+  }
+
+  updateFarm(id: string, farm: Farm): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, farm);
+  }
+
+  deleteFarm(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
--- Aquarius.Angular/src/app/services/pond.service.ts
+++ Aquarius.Angular/src/app/services/pond.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Pond } from '../models/pond.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PondService {
+  private apiUrl = 'https://localhost:5001/api/Ponds';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPonds(): Observable<Pond[]> {
+    return this.http.get<Pond[]>(this.apiUrl);
+  }
+
+  getPond(id: string): Observable<Pond> {
+    return this.http.get<Pond>(`${this.apiUrl}/${id}`);
+  }
+
+  createPond(pond: Pond): Observable<Pond> {
+    return this.http.post<Pond>(this.apiUrl, pond);
+  }
+
+  updatePond(id: string, pond: Pond): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, pond);
+  }
+
+  deletePond(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
