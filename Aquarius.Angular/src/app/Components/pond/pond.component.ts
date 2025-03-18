7d69cf9c9e84953ee0b2bf0eb9783ce04928499b
Brian Michel <bmoralescutting@gmail.com>
2025-03-18 00:02:09 -0400

    Components and services definition

---
+++ Aquarius.Angular/src/app/Components/pond/pond.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pond',
+  imports: [],
+  templateUrl: './pond.component.html',
+  styleUrl: './pond.component.css'
+})
+export class PondComponent {
+
+}
